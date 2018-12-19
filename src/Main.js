7c8fc1695ac59d8ed3bbb843d8231ef98b5eab27
Guilherme Gouveia Figueiredo Lima <ggfl@cin.ufpe.br>
2018-12-19 17:53:15 -0300

    Inputs

--- src/Main.js
+++ src/Main.js
@@ -1,12 +1,33 @@
-// Setup 
-var container = document.getElementById('container');
-var canvas = document.getElementById('canvas');
-var ctx = canvas.getContext('2d');
+// Setup
+var container;
+var canvas;
+var ctx;
+var mousePos;
+var movingPoint;
+var points;
+var moving;
+//Buttons and Input
+var iterations;
+var showPolygons;
+var showPoints;
+var recalcular;
+var reset;
 
-var mousePos = new Point(0, 0);
-var movingPoint = false;
-var points = [];
-iterations = 500;
+function setup() {
+    container = document.getElementById('container');
+    canvas = document.getElementById('canvas');
+    ctx = canvas.getContext('2d');
+    mousePos = new Point(0, 0);
+    movingPoint = false;
+    points = [];
+    moving = {'mov': false, 'circle':-1};
+    iterations = document.getElementById('Iterations').value;
+    showPolygons = document.getElementById('Polygons');
+    showPoints = document.getElementById('Points');
+    recalcular = document.getElementById('Recalcular');
+    reset = document.getElementById('Reset');
+}
+setup();
 
 function resizeCanvas() {
     canvas.width = parseFloat(window.getComputedStyle(canvas).width);
@@ -15,20 +36,65 @@ function resizeCanvas() {
 resizeCanvas();
 
 
+// Colision
+function circleClicked() {
+    for (let i = 0; i < points.length; i++) {
+        if (Vector.size(Vector.sub(mousePos, points[i])) <= 8) {
+            return i;
+        }
+    }
+    return -1;
+}
+
 // Events
 canvas.addEventListener('mousemove', e => {
     mousePos = new Point(e.offsetX, e.offsetY);
+    if (moving.mov) {
+        points[moving.circle] = mousePos;
+        draw();
+    }
 });
 
 canvas.addEventListener('mousedown', e => {
-    points.push(mousePos);
+    let checkClick = circleClicked();
+    if (checkClick > -1) {
+        moving.mov = true;
+        moving.circle = checkClick;
+    } else {
+        points.push(mousePos);
+        draw();
+    }
+});
+canvas.addEventListener('mouseup', e => {
+    if (moving.mov) {
+        moving.mov = false;
+        moving.circle = -1;
+    }
+});
+
+showPolygons.addEventListener('click', e=>{
     draw();
 });
+
+showPoints.addEventListener('click', e=>{
+    draw();
+});
+
+recalcular.addEventListener('click', e=>{
+    iterations = document.getElementById('Iterations').value;
+    draw();
+});
+
+reset.addEventListener('click', e=>{
+    setup();
+    draw();
+});
+
 
 // Draw stuff
 function circle(p) {
     ctx.beginPath();
-    ctx.fillStyle = "#99FF66";
+    ctx.fillStyle = "#0000CD";
     ctx.arc(p.x, p.y, 4, 0, 2 * Math.PI);
     ctx.fill();
     ctx.beginPath();
@@ -51,21 +117,27 @@ function curve(points) {
         curve.push(deCasteljau(points, i / iterations));
     }
     for (var i = 0; i < curve.length - 1; i++) {
-        line(curve[i], curve[i + 1], "#999966");
+        line(curve[i], curve[i + 1], "#E9967A");
     }
 };
 
+function polygonControl(points) {
+    for (let i = 0; i < points.length - 1; i++) {
+        line(points[i], points[i + 1], "#556B2F");
+    }
+}
+
 // Bezier
 function deCasteljau(points, t) {
-    if (points.length == 1){
+    if (points.length == 1) {
         return points[0];
     }
     var dc_points = [];
-    for (var i = 0; i < (points.length-1); i++){
+    for (var i = 0; i < (points.length - 1); i++) {
         // Do (1-t)*a + t*b
-        var a = Vector.kProduct(1-t, points[i]);
-        var b = Vector.kProduct(t, points[i+1]);
-        var c = Vector.add(a,b);
+        var a = Vector.kProduct(1 - t, points[i]);
+        var b = Vector.kProduct(t, points[i + 1]);
+        var c = Vector.add(a, b);
         dc_points.push(c);
     }
     return deCasteljau(dc_points, t);
@@ -73,11 +145,17 @@ function deCasteljau(points, t) {
 
 function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    points.forEach(point => {
-        circle(point);
-    });
 
+    if(showPoints.checked){
+        points.forEach(point => {
+            circle(point);
+        });
+    }
     if (points.length > 1) {
         curve(points);
+    }
+
+    if(showPolygons.checked){
+        polygonControl(points);
     }
 };
